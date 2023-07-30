094187b2feb9c483074e0fe93c6f4e11922a662b
Iurii Cherkasov <cherkasov1010@gmail.com>
2023-07-30 16:06:03 +0300

    Module 3 task 3

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first, second, third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.printf("%d\n%d\n%d", first, second, third);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first, second, third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.printf("%d\n%d\n%d\n", first, second, third);
+        int linkToFirst, linkToSecond, linkToThird;
+        linkToFirst = first;
+        linkToSecond = second;
+        linkToThird = third;
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.printf("%d\n%d\n%d\n%d\n%d\n%d", first, second, third, linkToFirst, linkToSecond, linkToThird);
     }
 }
